0e9c6ad9a458360cfbe7c50d9c4640473c76dd1a
Gabrieli <gabibrandalise20662@gmail.com>
2025-06-26 09:04:04 -0300

    mudanças menuprivado, adição logout

--- src/componentes/MenuPrivado.jsx
+++ src/componentes/MenuPrivado.jsx
@@ -16,14 +16,28 @@ function MenuPrivado() {
                      <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <NavLink className="nav-link active" aria-current="page" exact="true" to="/">CAB</NavLink>
+                            <NavLink className="nav-link active" aria-current="page" exact="true" to="/privado">CAB</NavLink>
+                            {usuario &&
                             <NavDropdown title="Opções" id="basic-nav-dropdown">
-                                <NavLink className="dropdown-item" exact="true" to="pedido">Pedidos</NavLink>
-                                <NavLink className="dropdown-item" exact="true" to="cliente">Clientes</NavLink>
-                                <NavLink className="dropdown-item" exact="true" to="produto">Produtos</NavLink>
+                                <NavLink className="dropdown-item" exact="true" to="/privado/pedido">Pedidos</NavLink>
+                                <NavLink className="dropdown-item" exact="true" to="/privado/cliente">Clientes</NavLink>
+                                <NavLink className="dropdown-item" exact="true" to="/privado/produto">Produtos</NavLink>
                             </NavDropdown>
+                            }
+
                             <NavLink className="nav-link active" aria-current="page" exact="true" to="agendamento">Agendamentos</NavLink>
                         </Nav>
+                    </Navbar.Collapse>
+                     <Navbar.Collapse className="justify-content-end">                        
+                        <NavDropdown title={usuario ? "Usuário: " + usuario.nome : "Usuário"} id="basic-nav-dropdown">
+                            {usuario ?
+                                <NavLink className="dropdown-item" exact="true"
+                                    to="/" onClick={() => logout()}>Logout</NavLink>
+                                :
+                                <NavLink className="dropdown-item" exact="true"
+                                    to="/login">login</NavLink>
+                            }
+                        </NavDropdown>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
